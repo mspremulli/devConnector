df000f625758c7bc09688df58700d5dcad97fdce
Michael Spremulli <michaels.wpi@gmail.com>
2020-05-19 20:56:14 -0400

    added delte user and delete profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -130,5 +130,24 @@ router.get('/user/:user_id', async (req, res) => {
 });
 
 
+//@route  DELETE api/profile/
+//@desc  delete profile, user, and posts
+//@access Private
+router.delete('/',auth, async (req, res) => {
+  try{
+    //todo:remove users posts once we have added posts in
+    //remove profile
+    await Profile.findOneAndRemove({user:req.user.id})
+    
+    //remove user
+    await User.findOneAndRemove({_id:req.user.id})
+
+    res.json({msg: 'user deleted'});
+  }
+  catch(err){
+    console.error(err.message);
+    res.status(500).send('Server error')
+  }
+});
 
 module.exports = router;
