127983d585147e3af4f77e13e392f6678e23d4d1
Michael Spremulli <michaels.wpi@gmail.com>
2020-05-19 20:44:25 -0400

    added get all users profiles and get single profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -96,8 +96,38 @@ router.post(
   }
 )
 
+//@route  GET api/profile/
+//@desc  get all profiles
+//@access Public
+router.get('/', async (req, res) => {
+  try{
+    const profiles = await Profile.find().populate('user',['name', 'avatar']);
+    res.json(profiles);
+  }
+  catch(err){
+    console.error(err.message);
+    res.status(500).send('Server error')
+  }
+});
 
+//@route  GET api/profile/user/:user_id
+//@desc  get one profile by user id
+//@access Public
+router.get('/user/:user_id', async (req, res) => {
+  try{
+    const profile = await Profile.findOne({user:req.params.user_id})
+    .populate('user',['name', 'avatar']);
+    if(!profile){
+      return res.starus(400).json({message: 'There is no profile for this user.'})
+    }
 
+    res.json(profile);
+  }
+  catch(err){
+    console.error(err.message);
+    res.status(500).send('Server error')
+  }
+});
 
 
 
