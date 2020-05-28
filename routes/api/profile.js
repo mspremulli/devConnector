c5a84589edea1126c45f1b12cd5f7f327162bf60
Michael Spremulli <michaels.wpi@gmail.com>
2020-05-27 23:19:36 -0400

    added route to add github user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const request = require('request');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 //@route  GET api/profile/me
@@ -307,5 +309,35 @@ router.delete('/education/:edu_id', auth, async(req, res) => {
     res.status(500).send('Server error deleting education')
   }
 });
+
+//@route  Get api/profile/github/:username
+//@desc  get users repos from github 
+//@access Public
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      url: `https:api/github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: {'user-agent': 'node.js'}
+
+
+    }
+    
+
+    request(options, (error, response, body) => {
+      if(error) console.error(error);
+      if(response.statusCode != 200){
+        res.status(404).json({msg:'No github profile found'});
+      }
+      res.json(JSON.parse(body));
+    })
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error adding github')
+  }
+});
+
+
+
 
 module.exports = router;
