a58f7c3e832daf3822ec8d7d9820448225976419
Michael Spremulli <michaels.wpi@gmail.com>
2020-06-01 01:36:53 -0400

    added login and register page links

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,34 @@
 import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css';
 import Navbar from './components/layouts/Navbar';
-import Navbar from './components/layouts/Landing';
 import Landing from './components/layouts/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => {
   return (
-   <Fragment>
-     <Navbar />
-     <Landing />
-   </Fragment>
-  );
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path ='/'>
+          <Landing />
+        </Route>
+        <section className = "container">
+          <Switch>
+            <Route exact path ='/login'>
+             <Login />
+            </Route>
+            <Route exact path ='/register'>
+             <Register />
+            </Route>
+          </Switch>
+        </section>
+        
+      </Fragment>
+    </Router>
+
+  ); 
 }
 
 export default App;
