c9983fc2b773b20574137a65c4a985219c742eb7
Michael Spremulli <michaels.wpi@gmail.com>
2020-06-16 01:31:05 -0400

    added user login fail and success actions and reducers

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,4 @@
-import {REGISTER_FAIL, REGISTER_SUCCESS} from '../actions/types';
+import {REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED, AUTH_ERROR} from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -11,6 +11,21 @@ export default function(state = initialState, action) {
   const {type, payload} = action;
 
   switch(type){
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload
+      }
+    case AUTH_ERROR:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false
+      }
     case REGISTER_SUCCESS:
       return {
         ...state,
