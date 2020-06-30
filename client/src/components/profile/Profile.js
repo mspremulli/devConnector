e446c7c61f536dd010bc41c2342b52c3e69569d6
Michael Spremulli <michaels.wpi@gmail.com>
2020-06-29 22:57:27 -0400

    added github profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation'
+import ProfileGithub from './ProfileGithub'
 import {getProfileById} from '../../actions/profile'
 
 const Profile =( {match, getProfileById, profile : {profile, loading }, auth }) => {
@@ -42,8 +43,12 @@ const Profile =( {match, getProfileById, profile : {profile, loading }, auth }) 
             {profile.experience.map(experience => {
               <ProfileEducation key = {education._id} education={education} />
             })}
-          </Fragment>) : (<h4>no education </h4>)}
+          </Fragment>) : (<h4>no education credentials</h4>)}
         </div>
+
+        {profile.githubusername && (
+          <ProfileGithub username={profile.githubusername}/>
+        )}
 
        </div>
       </Fragment>}
