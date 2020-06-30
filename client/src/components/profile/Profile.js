bd9b1eb12bc8110db3f1078063d5e6dc85f02e94
Michael Spremulli <michaels.wpi@gmail.com>
2020-06-29 22:42:07 -0400

    added education and expexperience components

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,8 @@ import {connect} from 'react-redux';
 import Spinner from '../layouts/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation'
 import {getProfileById} from '../../actions/profile'
 
 const Profile =( {match, getProfileById, profile : {profile, loading }, auth }) => {
@@ -25,6 +27,24 @@ const Profile =( {match, getProfileById, profile : {profile, loading }, auth }) 
        <div class="profile-grid my-1">
         <ProfileTop profile={profile} />
         <ProfileAbout profile={profile} />
+        <div className='profile-exp bg-white p-2'>
+          <h2 className='text-primary'>Experience</h2>
+          {profile.experience.length > 0 ? (<Fragment>
+            {profile.experience.map(experience => {
+              <ProfileExperience key = {experience._id} experience={experience} />
+            })}
+          </Fragment>) : (<h4>no experience </h4>)}
+        </div>
+
+        <div className='profile-exp bg-white p-2'>
+          <h2 className='text-primary'>Education</h2>
+          {profile.education.length > 0 ? (<Fragment>
+            {profile.experience.map(experience => {
+              <ProfileEducation key = {education._id} education={education} />
+            })}
+          </Fragment>) : (<h4>no education </h4>)}
+        </div>
+
        </div>
       </Fragment>}
     </Fragment>
